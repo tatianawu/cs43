036955e35116f56b4fafac4e4384983b635d1057
Tatiana Wu <tatiana.wu99@gmail.com>
2019-01-25 23:49:17 -0800

    Complete assignment 2

---
+++ assign2/src/Main.hs
@@ -0,0 +1,35 @@
+module Main where
+
+import Data.Semigroup (stimes) -- stimes not imported by default
+
+
+main :: IO ()
+main = do
+  putStrLn "hello world"
+
+
+newtype Product = Product Int deriving (Show)
+newtype Sum = Sum Int deriving (Show)
+
+instance Semigroup Sum where
+    (Sum a) <> (Sum b) = Sum $ a + b -- define <>
+    
+    stimes n (Sum x)
+      | n <= 0    = error "positive multiplier expected"
+      | otherwise = Sum $ x * (fromIntegral n)
+    
+instance Semigroup Product where
+    (Product a) <> (Product b) = Product $ a * b
+    
+    stimes n (Product x)
+      | n <= 0    = error "positive multiplier expected"
+      | otherwise = Product $ x ^ (fromIntegral n)
+
+
+stimes' :: (Semigroup a, Integral b) => b -> a -> a
+stimes' n x
+  | n <= 0    = error "positive multiplier expected"
+  | n == 1    = x
+  | even n    = stimes' (n `div` 2) x <> stimes' (n `div` 2) x
+  | otherwise = stimes' (n `div` 2 + 1) x <> stimes' (n `div` 2) x
+
